0760fcafed4656dbfb180d9d61fef39bf6bf31b3
Rob Verplanke <rverplanke@gmail.com>
2026-05-21 15:28:18 +0200

    styling: create and add needed components with styling

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,5 @@
+import styles from './Card.module.css'; // CSS module
+
+export default function Card() {
+  return <div className={styles.card}>Card</div>;
+}
--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,7 +1,47 @@
+import Card from '../Card/Card'; // Card component
+import styles from './GameBoard.module.css'; // CSS module
+
 export default function GameBoard() {
   return (
     <>
-      <h3>GameBoard component</h3>
+      <section className={styles.gameboard}>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+        </div>
+      </section>
     </>
   );
 }
