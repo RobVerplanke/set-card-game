435da6ee8cb20fe3b5059e4e5a2737c50a6d38c7
Rob Verplanke <rverplanke@gmail.com>
2026-06-01 15:14:04 +0200

    chore: add logs to store methods for testing

--- src/components/elements/header/scoreBoard/ScoreBoard.tsx
+++ src/components/elements/header/scoreBoard/ScoreBoard.tsx
@@ -1,9 +1,13 @@
 import styles from '../ScoreBoard/ScoreBoard.module.css';
+import { useGameStore } from '../../../../store/gameStore';
 
 export default function ScoreBoard() {
+  // Get score from store
+  const score = useGameStore((state) => state.score);
+
   return (
     <div className={styles.ScoreBoard}>
-      <span className={styles.score}>Score: 0</span>
+      <span className={styles.score}>Score: {score}</span>
       <br />
       <span className={styles.penalties}>Pentalties: 0</span>
     </div>
--- src/store/gameStore.ts
+++ src/store/gameStore.ts
@@ -20,44 +20,59 @@ export const useGameStore = create<GameStore>((set, get) => ({
   // Card actions
   selectCard: () => {},
   initializeGame: () => {
+    console.log('Game initialized!');
     const shuffled = shuffleDeck(generateDeck());
     set({
       deck: shuffled.slice(12),
       board: shuffled.slice(0, 12),
-      phase: 'playing',
     });
   },
-  replaceCards: () => {},
-  addCards: () => {},
+
+  replaceCards: () => {
+    console.log('Cards replaced');
+  },
+
+  addCards: () => {
+    console.log('Cards added');
+  },
 
   // Score actions
-  incrementScore: () =>
+  incrementScore: () => {
+    console.log('score + 1');
     set((state) => ({
       score: state.score + 1,
-    })),
+    }));
+  },
 
-  registerInvalidSet: () =>
+  registerInvalidSet: () => {
+    console.log('Invalid set!');
     set((state) => ({
       score: state.score - 1,
       invalidSets: state.invalidSets + 1,
-    })),
+    }));
+  },
 
-  useHint: () =>
+  useHint: () => {
+    console.log('Used hint');
     set((state) => ({
       hintsUsed: state.hintsUsed + 1,
       score: state.score - 1,
-    })),
+    }));
+  },
 
   // Game actions
   startGame: () => {
-    console.log('start new game!');
+    console.log('Start new game!');
     get().initializeGame();
     set({
       phase: 'playing',
     });
   },
-  endGame: () => {},
-  resetGame: () =>
+  endGame: () => {
+    console.log('Game ended!');
+  },
+  resetGame: () => {
+    console.log('Reset game!');
     set({
       deck: [],
       board: [],
@@ -66,5 +81,6 @@ export const useGameStore = create<GameStore>((set, get) => ({
       hintsUsed: 0,
       invalidSets: 0,
       phase: 'idle',
-    }),
+    });
+  },
 }));
